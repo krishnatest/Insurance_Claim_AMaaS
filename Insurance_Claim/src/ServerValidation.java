2a7b135dd45af76e723b255b868d689497df7700
Krishna <kkumar92@csc.com>
2016-10-03 17:39:55 +0530

    Error

--- Insurance_Claim/src/ServerValidation.java
+++ Insurance_Claim/src/ServerValidation.java
@@ -72,7 +72,6 @@ public class ServerValidation extends HttpServlet {
 		//if (ClaimSubmit.equals("")) {
 			error += "Please Select the ClaimSubmit<br/>";
 			result = false;
-
 		}
 
 		if (Fname.equals("")) {
